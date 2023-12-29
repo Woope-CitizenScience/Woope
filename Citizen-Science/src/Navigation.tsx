818ac5c631d77c4e28a0e1f02edffbaaa60a9a69
Robert Khalulyan <robert@Roberts-MacBook-Pro-4.local>
2023-12-29 13:20:12 -0800

    Changed code structure a bit for organization. Added a few more dependencies"

---
+++ Citizen-Science/src/Navigation.tsx
@@ -0,0 +1,12 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import '/src/screens/Welcome-Screen';
+import '/src/screens/Login-Screen';
+import '/src/screens/Signup-Screen';
+
+const Stack = createNativeStackNavigator();
+export default AppNavigator = () => {
+    return(
+        //TODO Remove text and implement actual navigation
+        <>Input navigation screens</>
+    );
+};
---
+++ Citizen-Science/src/StyleSheet.tsx
@@ -0,0 +1,11 @@
+// Refer to the following StyleSheet Documentation:
+// https://reactnative.dev/docs/stylesheet
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const styles = StyleSheet.create({
+    title: {},
+    container: {},
+    threads:{}
+})
