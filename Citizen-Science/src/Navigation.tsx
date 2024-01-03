f6fc600e4f6648d04a15e9f0be70805ca3920cef
Robert Khalulyan <robert@Roberts-MacBook-Pro-4.local>
2024-01-03 15:33:39 -0800

    Added the fade transition to the spash screen as well as adding a swipe up transition to login

--- Citizen-Science/src/Navigation.tsx
+++ Citizen-Science/src/Navigation.tsx
@@ -2,15 +2,26 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './screens/WelcomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
-
+import SplashScreen from "./screens/SplashScreen";
 
 const Stack = createNativeStackNavigator();
 const AppNavigation = () => {
     return(
-        <Stack.Navigator initialRouteName={"Welcome"} screenOptions={{headerShown: false}}>
-            <Stack.Screen name="Welcome" component={WelcomeScreen}/>
-            <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Navigator
+            initialRouteName={"Splash"}
+            screenOptions={{headerShown: false}}>
+            <Stack.Screen
+                name="Welcome"
+                component={WelcomeScreen}
+                options={{animation: 'fade'}}
+            />
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{animation: 'slide_from_bottom'}}
+            />
             <Stack.Screen name="Signup" component={SignupScreen}/>
+            <Stack.Screen name="Splash" component={SplashScreen}/>
         </Stack.Navigator>
     );
 };
