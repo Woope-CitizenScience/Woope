fb280f7386b18d6c783c208a82b3ca60ca3ce5e4
raneenkakar <raneenkhan.12@gmail.com>
2024-01-10 17:33:10 -0800

    Updated Navigation

--- Citizen-Science/src/components/NavigationBar.tsx
+++ Citizen-Science/src/components/NavigationBar.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
 import Svg, { Path } from 'react-native-svg';
 import { mdiHome, mdiTestTube, mdiCalendar, mdiBookshelf, mdiMapMarker } from '@mdi/js';
 import HomeScreen from '../screens/HomeScreen';
@@ -9,50 +11,85 @@ import ResourceScreen from '../screens/ResourceScreen';
 import CitizenScienceScreen from '../screens/CitizenScience';
 
 const Tab = createBottomTabNavigator();
+interface AnimatedTabIconProps {
+    focused: boolean;
+    IconPath: string;
+}
+const AnimatedTabIcon: React.FC<AnimatedTabIconProps> = ({ focused, IconPath }) => {
+    const scale = useSharedValue(1);
+
+    useEffect(() => {
+        scale.value = withTiming(focused ? 1.5 : 1);
+    }, [focused]);
+
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{ scale: scale.value }],
+        };
+    });
+
+    return (
+        <Animated.View style={animatedStyle}>
+            <Svg height="24" width="24" viewBox="0 0 24 24">
+                <Path fill={focused ? '#007AFF' : 'black'} d={IconPath} />
+            </Svg>
+        </Animated.View>
+    );
+};
 
 const NavigationBar = () => {
     return (
-        <Tab.Navigator
-            screenOptions={({ route }) => ({
-                headerShown: false,
-                tabBarIcon: ({ color, size }) => {
-                    let IconPath;
-                    switch (route.name) {
-                        case 'Home':
-                            IconPath= mdiHome;
-                            break;
-                        case 'CitizenScience':
-                            IconPath = mdiTestTube;
-                            break;
-                        case 'Calendar':
-                            IconPath = mdiCalendar;
-                            break;
-                        case 'Resource':
-                            IconPath = mdiBookshelf;
-                            break;
-                        case 'Map':
-                            IconPath = mdiMapMarker;
-                            break;
-                        default:
-                            IconPath = mdiHome;
-                    }
+        <View style={{ flex: 1 }}>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        let IconPath;
+                        switch (route.name) {
+                            case 'Home':
+                                IconPath= mdiHome;
+                                break;
+                            case 'CitizenScience':
+                                IconPath = mdiTestTube;
+                                break;
+                            case 'Calendar':
+                                IconPath = mdiCalendar;
+                                break;
+                            case 'Resource':
+                                IconPath = mdiBookshelf;
+                                break;
+                            case 'Map':
+                                IconPath = mdiMapMarker;
+                                break;
+                            default:
+                                IconPath = mdiHome;
+                        }
+                        return <AnimatedTabIcon focused={focused} IconPath={IconPath} />;
+                    },
+                    tabBarActiveTintColor: 'blue',
+                    tabBarInactiveTintColor: 'black',
+                    tabBarStyle:{
+                        backgroundColor: 'lightblue',
+                        paddingBottom: 13,
+                        paddingTop: 2,
+                        height: 80,
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20,
 
-                    return (
-                        <Svg height={size} width={size} viewBox="0 0 24 24">
-                            <Path fill={color} d={IconPath} />
-                        </Svg>
-                    );
-                },
-                tabBarActiveTintColor: 'tomato',
-                tabBarInactiveTintColor: 'gray',
-            })}
-        >
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="CitizenScience" component={CitizenScienceScreen} />
-            <Tab.Screen name="Calendar" component={CalendarScreen} />
-            <Tab.Screen name="Resource" component={ResourceScreen} />
-            <Tab.Screen name="Map" component={MapScreen} />
-        </Tab.Navigator>
+                    },
+                    tabBarLabelStyle: {
+                        marginBottom: 3,
+                    },
+                })}
+            >
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="CitizenScience" component={CitizenScienceScreen} />
+                <Tab.Screen name="Calendar" component={CalendarScreen} />
+                <Tab.Screen name="Resource" component={ResourceScreen} />
+                <Tab.Screen name="Map" component={MapScreen} />
+            </Tab.Navigator>
+        </View>
     );
 };
+
 export default NavigationBar;
