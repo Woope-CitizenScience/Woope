c5894e28c951e32e1e1e4fb5c0dc97bff8283db9
dentifrag <65632734+dentifrag@users.noreply.github.com>
2024-01-21 22:50:00 -0800

    JWT flow
    has a access token for general access (expires every 15m), and a refresh token to generate new access tokens when they expire.

--- api/migrations/sqls/20231212074355-create-users-up.sql
+++ api/migrations/sqls/20231212074355-create-users-up.sql
@@ -1,18 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 -- Create Users Table
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
+CREATE TABLE users
+(
+    user_id       SERIAL PRIMARY KEY,
     password_hash VARCHAR(255) NOT NULL,
-    email VARCHAR(100) UNIQUE,
-    phone_number VARCHAR(15) UNIQUE,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    last_login TIMESTAMP WITH TIME ZONE
+    email         VARCHAR(100) UNIQUE,
+    phone_number  VARCHAR(15) UNIQUE,
+    created_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    last_login    TIMESTAMP WITH TIME ZONE,
+    is_Admin      BOOLEAN DEFAULT FALSE,
+    refresh_token VARCHAR(255) UNIQUE
 );
 
 -- Create Profile Information Table
-CREATE TABLE profile_information (
-    profile_id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE REFERENCES users(user_id),
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
+CREATE TABLE profile_information
+(
+    profile_id    SERIAL PRIMARY KEY,
+    user_id       INT UNIQUE REFERENCES users (user_id),
+    first_name    VARCHAR(100),
+    last_name     VARCHAR(100),
     date_of_birth DATE
 );
---
+++ api/src/middleware/authMiddleware.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { User } from "../interfaces/User";
+import { config } from "../config/config";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+  }
+}
+
+export const authenticateToken = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, config.accessTokenSecret, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user as User;
+    next();
+  });
+};
+
+// Usage example:
+// router.get('/protected-route', authenticateToken, (req, res) => {
+//   // Access req.user here
+// });
