797949104a28d9dc673099b0ed3889b3149bb548
dentifrag <65632734+dentifrag@users.noreply.github.com>
2024-01-21 22:50:00 -0800

    JWT flow
    has a access token for general access (expires every 15m), and a refresh token to generate new access tokens when they expire.

--- api/src/routes/health.ts
+++ api/src/routes/health.ts
@@ -3,7 +3,11 @@ import { authenticateToken } from '../middleware/authMiddleware';
 const router = require('express').Router();
 
 router.get('/', (req: express.Request, res: express.Response) => {
-  res.send('OK');
+  res.send('Authenticated!');
+});
+
+router.get('/protected-route', authenticateToken, (req: express.Request, res: express.Response) => {
+  res.send('Protected!');
 });
 
 router.get('/protected-route', authenticateToken, (req: express.Request, res: express.Response) => {
