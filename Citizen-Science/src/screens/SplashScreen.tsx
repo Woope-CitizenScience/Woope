7738ce435894a87c8ef37ea5bda2a241b51e40bf
raneenkakar <raneenkhan.12@gmail.com>
2024-02-01 12:09:59 -0800

    useEffect
    As per Dakota's request, I pasted what you asked for. Thanks.

--- Citizen-Science/src/screens/SplashScreen.tsx
+++ Citizen-Science/src/screens/SplashScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { View, ImageBackground, Dimensions } from 'react-native';
-import { useNavigation, NavigationContainerRef } from '@react-navigation/native';
+import { useNavigation, NavigationContainerRef, CommonActions } from '@react-navigation/native';
 
 // Get full width and height of the device's screen
 const screenWidth = Dimensions.get('window').width;
@@ -12,7 +12,12 @@ function SplashScreen() {
     useEffect(() => {
         // Splash screen for 2 seconds
         const splashTimer = setTimeout(() => {
-            navigation.navigate('Welcome'); // This will take us to welcome screen
+            navigation.dispatch(
+                CommonActions.reset({
+                    index: 0,
+                    routes: [{ name: 'Welcome' }],
+                })
+            );
         }, 2000);
 
         return () => clearTimeout(splashTimer); // This will cancel our splash screen after 2 seconds
