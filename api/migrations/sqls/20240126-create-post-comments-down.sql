d9c63f46eb9da263e938d9cb569f3c27f6ed3ff9
Josue Sanchez <30679533+jsanchez2388@users.noreply.github.com>
2024-02-06 19:21:13 -0800

    fix: updates made to db migrations. - Added weather_observations table to store weather data - Added comment_likes table to track likes - Updated triggers and functions

--- api/migrations/sqls/20240126-create-post-comments-down.sql
+++ api/migrations/sqls/20240126-create-post-comments-down.sql
@@ -1,17 +1,16 @@
--- Drop Posts Table
-DROP TABLE IF EXISTS posts;
-
--- Drop Comments Table
-DROP TABLE IF EXISTS comments;
-
--- Drop the likes_count_trigger from the comment_likes table
-DROP TRIGGER IF EXISTS comments_count_trigger ON comments;
-
--- Drop the update_likes_count function
-DROP FUNCTION IF EXISTS update_comments_count();
-
--- Drop the likes_count_trigger from the comment_likes table
+-- Drop Triggers
 DROP TRIGGER IF EXISTS likes_count_trigger ON comment_likes;
+DROP TRIGGER IF EXISTS comments_count_trigger ON comments;
+DROP TRIGGER IF EXISTS before_comment_delete ON comments;
+DROP TRIGGER IF EXISTS post_update_trigger ON posts;
 
--- Drop the update_likes_count function
+-- Drop Functions
 DROP FUNCTION IF EXISTS update_likes_count();
+DROP FUNCTION IF EXISTS update_comments_count();
+DROP FUNCTION IF EXISTS soft_delete_comment();
+DROP FUNCTION IF EXISTS set_post_is_updated();
+
+-- Drop Tables
+DROP TABLE IF EXISTS comment_likes CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
--- api/migrations/sqls/20240126-create-post-comments-up.sql
+++ api/migrations/sqls/20240126-create-post-comments-up.sql
@@ -19,34 +19,67 @@ CREATE TABLE comments (
     user_id INTEGER NOT NULL,
     content TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    likes_count INTEGER DEFAULT 0,
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
-    FOREIGN KEY (post_id) REFERENCES posts(post_id),
+    deleted_at TIMESTAMP,
+    likes_count INTEGER DEFAULT 0,
+    FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
     FOREIGN KEY (parent_comment_id) REFERENCES comments(comment_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
--- Create function to update the comments_count when a new comment is added
+-- Create comment_likes Table
+CREATE TABLE comment_likes (
+    like_id SERIAL PRIMARY KEY,
+    comment_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (comment_id) REFERENCES comments(comment_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- Create function to update the comments_count when a comment is added or deleted
 CREATE OR REPLACE FUNCTION update_comments_count()
 RETURNS TRIGGER AS $$
 BEGIN
     -- If a new comment is added, increment the comments_count of the respective post
     IF TG_OP = 'INSERT' AND NEW.is_active THEN
-        UPDATE posts SET comments_count = comments_count + 1 WHERE post_id = NEW.post_id;
-    -- If a comment is deleted (marked as inactive), decrement the comments_count
-    ELSIF TG_OP = 'UPDATE' AND NEW.is_active IS DISTINCT FROM OLD.is_active THEN
-        UPDATE posts
-        SET comments_count = comments_count + CASE WHEN NEW.is_active THEN 1 ELSE -1 END
+        UPDATE posts SET comments_count = comments_count + 1 
         WHERE post_id = NEW.post_id;
+    -- If a comment is deleted, decrement the comments_count of the respective post
+    ELSIF TG_OP = 'DELETE' THEN
+        UPDATE posts
+        SET comments_count = comments_count - 1
+        WHERE post_id = OLD.post_id;
     END IF;
-    RETURN NEW;
+    RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
 
--- Create trigger that calls the update_comments_count function after insert or update of a comment
+-- Create function to handle user deletion of comments
+CREATE OR REPLACE FUNCTION soft_delete_comment()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Mark the comment as inactive, set the deletion message, and record the deletion time
+    UPDATE comments
+    SET is_active = FALSE,
+        content = 'Comment has been deleted',
+        deleted_at = CURRENT_TIMESTAMP
+    WHERE comment_id = OLD.comment_id;
+
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create trigger that calls the update_comments_count function after user inserts a comment
 CREATE TRIGGER comments_count_trigger
-AFTER INSERT OR UPDATE OF is_active ON comments
+AFTER INSERT ON comments
 FOR EACH ROW EXECUTE FUNCTION update_comments_count();
+
+-- Create trigger that calls soft_delete_comment function after user deletes a comment
+CREATE TRIGGER before_comment_delete
+BEFORE DELETE ON comments
+FOR EACH ROW
+EXECUTE FUNCTION soft_delete_comment();
 
 -- Create function to update the likes_count when a like is added to or removed from a comment
 CREATE OR REPLACE FUNCTION update_likes_count()
@@ -68,3 +101,20 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER likes_count_trigger
 AFTER INSERT OR DELETE ON comment_likes
 FOR EACH ROW EXECUTE FUNCTION update_likes_count();
+
+-- Create function to set is_updated to true if a post has been updated
+CREATE OR REPLACE FUNCTION set_post_is_updated()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF OLD.content <> NEW.content THEN
+        NEW.is_updated = TRUE;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create trigger that calls the set_post_is_updated function after a post has been updated
+CREATE TRIGGER post_update_trigger
+BEFORE UPDATE ON posts
+FOR EACH ROW
+EXECUTE FUNCTION set_post_is_updated();
