afb546bd36069b96cf7e4cf5d1ac5d2035b73738
dentifrag <65632734+dentifrag@users.noreply.github.com>
2024-02-16 00:51:08 -0800

    added example for how to authenticate screen

--- Citizen-Science/src/screens/CalendarScreen.tsx
+++ Citizen-Science/src/screens/CalendarScreen.tsx
@@ -1,16 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {fetchWithToken} from '../util/fetchWithToken';
 import {useIsFocused} from "@react-navigation/native";
+import {AuthContext} from "../util/AuthContext";
 
 const CalendarScreen = () => {
 	const isFocused = useIsFocused();
+	const { userToken, setUserToken } = useContext(AuthContext);
 	// testing fetchWithToken
 	useEffect(() => {
 		if (isFocused) {
 			const fetchData = async () => {
-				const response = await fetchWithToken(`${process.env.EXPO_PUBLIC_API_URL}/health/protected-route`);
-				console.log('data:', await response.text());
+				const { response, newAccessToken, tokenRefreshFailed } = await fetchWithToken(`${process.env.EXPO_PUBLIC_API_URL}/health/protected-route`);
+				if (newAccessToken && newAccessToken !== userToken) {
+					setUserToken(newAccessToken);
+				} else if (tokenRefreshFailed) {
+					setUserToken(null);
+				}
 			};
 			fetchData();
 		}
