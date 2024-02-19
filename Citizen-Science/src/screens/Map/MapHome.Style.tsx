4049aeda5bb6a2b84921e886729c8846007937cd
dentifrag <65632734+dentifrag@users.noreply.github.com>
2024-02-19 12:53:38 -0800

    Refactoring existing map stuff to work with current verison of code

---
+++ Citizen-Science/src/screens/Map/MapHome.Style.tsx
@@ -0,0 +1,18 @@
+import {StyleSheet} from "react-native";
+
+export const HomePage = StyleSheet.create({
+	content: {
+		display : "flex",
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		flexDirection: "row",
+		backgroundColor: "#5EA1E9",
+	},
+	view: {
+		width: "80%"
+	},
+	cardButton: {
+		color: "#5EA1E9",
+	}
+})
---
+++ Citizen-Science/src/screens/Map/MapHomeScreen.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { SafeAreaView, ScrollView } from 'react-native';
+import {Appbar} from 'react-native-paper';
+import {Card, Button} from 'react-native-paper';
+import * as Location from 'expo-location';
+import {useState} from 'react';
+import {HomePage} from './MapHome.Style'
+
+interface MapPageScreenProps {
+	navigation: any;
+}
+
+export const MapHome = (props: MapPageScreenProps) => {
+	const [location, setLocation] =useState({})
+	const getLocation = () => {
+		(async() => {
+			let {status} = await Location.requestForegroundPermissionsAsync()
+			if (status == 'granted'){
+				console.log('Permission successful!')
+			} else {
+				console.log('Permission denied')
+			}
+			const loc = await Location.getCurrentPositionAsync()
+			console.log(loc)
+			setLocation(loc)
+		})()
+	};
+	const ViewMap = () => props.navigation.navigate('MapScreen');
+	const ViewSMap = () => props.navigation.navigate('SMapScreen');
+
+	return(
+		<SafeAreaView style = {HomePage.content}>
+			<ScrollView style = {HomePage.view}>
+				<Appbar>
+					<Appbar.BackAction />
+					<Appbar.Content title = "Map"/>
+				</Appbar>
+				<Card>
+					<Card.Content>
+						<Button
+							mode="contained"
+							onPress={() => {
+								getLocation();
+								ViewMap();
+							}}
+						>Place pin</Button>
+						<Button
+							mode="contained"
+							onPress={() => {
+								ViewSMap();
+							}}
+						>View Map</Button>
+					</Card.Content>
+				</Card>
+			</ScrollView>
+		</SafeAreaView>
+	);
+}
