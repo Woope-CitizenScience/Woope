5eb27a3be718ee103152c5a066d7fd3b85de2ae6
hii314 <35716033+hii314@users.noreply.github.com>
2024-02-27 22:14:29 -0800

    Revert "merged test into MapIntegration refactored code to work with test"
    This reverts commit 5ade1b5c2598bbe5d7018de9895d6b4ce0816eb5.

--- Citizen-Science/App.tsx
+++ Citizen-Science/App.tsx
@@ -1,20 +1,35 @@
-import {StyleSheet, Text, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import AppNavigation from '../Citizen-Science/src/Navigation';
-import {AuthProvider} from './src/util/AuthContext';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from "./screens/Login";
+import Home from "./screens/Home";
 import * as React from "react";
 import { MapScreen } from './screens/Map/Map.Screen';
 import { MapHome } from './screens/MapHome/MapHome.Screen';
 import { SMapScreen } from './screens/Map/SMap.Screen';
 
 
+const Stack = createNativeStackNavigator();
 export default function App() {
-	return (
-		<AuthProvider>
-			<NavigationContainer>
-				<AppNavigation/>
-			</NavigationContainer>
-		</AuthProvider>
-	);
+  return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen options={{headerShown: false}} name="Login" component={Login} />
+          <Stack.Screen options={{headerShown: false}} name="Home" component={Home} />
+          <Stack.Screen options={{headerShown: false}} name="MapHome" component={MapHome} />
+          <Stack.Screen options={{headerShown: false}} name="MapScreen" component={MapScreen} />
+          <Stack.Screen options={{headerShown: false}} name="SMapScreen" component={SMapScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
