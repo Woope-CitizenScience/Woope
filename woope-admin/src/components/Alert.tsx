655510ce67c17b0ac3470eabd8f2dde73bb791a6
krooton <104192072+krooton@users.noreply.github.com>
2025-01-03 09:51:32 -0800

    Initial commit for admin website

---
+++ woope-admin/src/components/Alert.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible" role="alert">
+      {children}
+      <button
+        type="button"
+        className="close"
+        data-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
+};
+
+export default Alert;
