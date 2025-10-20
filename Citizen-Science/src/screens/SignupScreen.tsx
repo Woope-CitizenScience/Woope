c4d0031c456759bed573da723514429033a25f75
Christian Herrera <77373551+ChristianHerrera-me@users.noreply.github.com>
2025-10-20 10:42:03 -0700

    fixed otp textbar

--- Citizen-Science/src/screens/SignupScreen.tsx
+++ Citizen-Science/src/screens/SignupScreen.tsx
@@ -349,7 +349,13 @@ const SignupScreen = () => {
 							onChangeText={(value) => setOtp(value)}
 							borderColor="#5EA1E9"
 							borderRadius={10}
-							position={{ top: 17, left: 0 }}
+							//testing code below
+							position={{ top: 3, left: 0 }} // Adjust position of OTP field
+							textContentType={'oneTimeCode'} //testing
+							autoFocus={true} //auto focus on OTP field when it appears
+							keyboardType ="number-pad"	//changes keyboard to number pad
+
+
 						/>
 					)}
 					{/* Signup Button */}
